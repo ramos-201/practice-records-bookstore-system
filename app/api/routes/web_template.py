c4f5ac202ad26eac6cd486ec36c9983173e41360
Ramos <andfera201@gmail.com>
2023-05-02 22:24:24 -0500

    fix: add home.html

--- app/api/routes/web_template.py
+++ app/api/routes/web_template.py
@@ -1,5 +1,7 @@
-from starlette.responses import PlainTextResponse
+from starlette.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory='app/views/templates')
 
 
 def home(request):
-    return PlainTextResponse('Hello, world!')
+    return templates.TemplateResponse('index.html', {'request': request, 'response': {'title': 'Home'}})
